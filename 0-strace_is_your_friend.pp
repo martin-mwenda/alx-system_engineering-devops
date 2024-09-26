eca592d20742f0468d93094bc2b837efd79669de
martin <mwirigi.8kabush@gmail.com>
2024-09-25 19:30:36 -0700

    apache in place

---
+++ 0-strace_is_your_friend.pp
@@ -0,0 +1,16 @@
+#Puppet code to ensure the Apache configuration is in place
+
+file { '/etc/apache2/sites-enabled/000-default.conf':
+  ensure  => present,
+  source  => 'puppet:///modules/apache/000-default.conf',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  require => Package['apache2'],
+}
+
+service { 'apache2':
+  ensure     => running,
+  enable     => true,
+  subscribe  => File['/etc/apache2/sites-enabled/000-default.conf'],
+}
